c42f18b72f381acceece5a14ae6c99886f8b6903
Housni Alami <alami.housni@gmail.com>
2020-04-29 20:52:04 +0200

    edit profile page in progress

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,51 +1,72 @@
 import React, { Component } from "react";
 import "./userProfile.css";
-import "./meme.jpg";
+import meme from "./meme.jpg";
 
 class UserProfile extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
             data: {
-                id: 3,
+                id: 2,
                 name: "undefined user name",
                 email: "undefined email",
-                points: 0,
-                average_rating: "0"
-            }
+                isInEditMode: false
+            },
+        text:"Edit"
         }
+        this.newName = "";
+        this.newEmail = "";
     };
-   
+
+    changeEditMode=() =>  {
+       this.setState({ isInEditMode: !this.state.isInEditMode })
+       let text=this.state.isInEditMode ? "Edit" : "Save";
+       this.setState({text:text})
+
+    };
+
     componentWillMount() {
         let userid = this.state.data.id;
         fetch(`http://localhost:5000/users/${userid}`)
             .then((response) => response.json())
             .then((responseJson) => {
-              this.setState({ data : responseJson })
+                this.setState({ data: responseJson })
             })
             .catch((error) => {
-              console.error(error);
+                console.error(error);
             });
-    } 
+    }
 
     render() {
+        
         return (
-            <>
-                <div className="userProfile">
-                    <div className="myProfile-bar">
-                        <p>My Profile</p>
-                    </div>
-                    <div className="infos">
-                        <img src="meme.jpg" alt="image user" />
-                        <div>
-                            <div className="userName">{this.state.data.name}</div>
 
-                            <div className="email">{this.state.data.email}</div>
-                        </div>
-                    </div>
+            <div className="userProfile">
+                <div className="myProfile-bar">
+                    <p>My Profile</p>
                 </div>
-            </>
+                <button className="edit-save-btn" onClick={this.changeEditMode}>{this.state.text}</button>
+
+                <div className="infos">
+                    <img src={meme} alt="image user" />
+                    <div>
+                        {!this.state.isInEditMode ? (
+                            <div className="userName">{this.state.data.name}</div>
+                        ) : (
+                                <input type="text" defaultValue={this.state.data.name} ref={node => { this.newName = node }}/>
+                               
+                            )}
+                            {!this.state.isInEditMode ? (
+                        <div className="email">{this.state.data.email}</div>
+                        ) : (
+                            <input type="text" defaultValue={this.state.data.email} ref={node => { this.newEmail = node }} />
+                        )}
+                    </div>
+                    
+                </div>
+            </div>
+
 
         )
     }
