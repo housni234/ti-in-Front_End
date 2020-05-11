ef7de95a7b9bbe9493c643591b8aed8875f68cef
rahaf89 <rahaflh.900@hotmail.com>
2020-05-11 21:51:10 +0200

    logo

--- src/TieInBar.js
+++ src/TieInBar.js
@@ -3,21 +3,30 @@ import "./main.css";
 import { Link } from "react-router-dom";
 
 class TieInBar extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="sidebar tie-in base">
-                    <h1>TIE-IN</h1>
-                    <div className="bar-container">
-                        <div className="links-container">
-                            <Link to="/Profile">Profile</Link>
-                            <Link to="">Services</Link>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                )
-            }
-        };
-        
+  render() {
+    return (
+      <div className="container">
+        <div className="sidebar tie-in base">
+        <br />
+          <br />
+          <img
+            src="TieIn.png"
+            width="50px"
+            height="50px"
+            className="logo"
+            alt="logo"
+          />{"TieIn"}
+          <br />
+          <h1>Good Neighbour</h1>
+          <div className="bar-container">
+            <div className="links-container">
+              <Link to="/Profile">Profile</Link>
+            </div>s
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default TieInBar;
